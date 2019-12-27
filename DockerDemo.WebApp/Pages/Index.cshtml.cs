7581e7665424ec51ae73b9e230e49ed0105449dd
Raj Kumar1 <Raj.Kumar1@rsystems.com>
2019-12-27 14:58:58 +0530

    Initial commit

---
+++ DockerDemo.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace DockerDemo.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IDistributedCache _distributedCache;
+        public IndexModel(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+        public void OnGet()
+        {
+            string cacheData = string.Empty;
+            var cacheKey = "TheTime";
+            var existingTime = _distributedCache.GetString(cacheKey);
+            if (!string.IsNullOrEmpty(existingTime))
+            {
+                cacheData =  "Fetched from cache : " + existingTime;
+            }
+            else
+            {
+                existingTime = DateTime.UtcNow.ToString();
+                _distributedCache.SetString(cacheKey, existingTime);
+                cacheData = "Added to cache : " + existingTime;
+            }
+
+            ViewData["CacheData"] = cacheData;
+        }
+    }
+}
